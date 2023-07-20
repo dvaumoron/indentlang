fc047dd7335b32ebb52109f5306112b373d94cc2
dvaumoron <dvaumoron@gmail.com>
2023-07-20 08:47:53 +0200

    move package comment

--- adapter/gin/adapter.go
+++ adapter/gin/adapter.go
@@ -16,6 +16,11 @@
  *
  */
 
+/*
+Package ginadapter implements LoadTemplatesAsRender to supply a gin compatible HTMLRender.
+
+See https://gin-gonic.com/ for more information about gin.
+*/
 package ginadapter
 
 import (
--- adapter/gin/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package ginadapter implements LoadTemplatesAsRender to supply a gin compatible HTMLRender.
-
-See https://gin-gonic.com/ for more information about gin.
-*/
-package ginadapter
